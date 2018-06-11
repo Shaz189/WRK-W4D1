75533dabf27dc72852237f661c5b2c8717cf1249
daniel <dpark189@gmail.com>
2018-06-11 14:28:40 -0400

    Finish first_routes

---
+++ first_routes/db/migrate/20180611175256_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string "name", null: false
+      t.string "email", null: false
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
+  end
+end
